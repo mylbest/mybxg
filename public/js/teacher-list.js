363c71a4d96f8238003e0e1a1b3ed848e95d6ae9
mylbest <948596867@qq.com>
2017-08-31 17:35:39 +0800

    完成讲师启用注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -30,6 +30,28 @@ define(['jquery','template','bootstrap'],function($,template){
       		}
       	})
       })
+      $('.eod').click(function(){
+      	// console.log(12345);
+      	var td=$(this).closest('td')
+      	var tcId=td.attr('data-tcId');
+      	var tcStatus=td.attr('data-status');
+      	var that=this;
+      	$.ajax({
+      		type:'post',
+      		url:'/api/teacher/handle',
+      		data:{tc_id:tcId,tc_status:tcStatus},
+      		dataType:'json',
+      		success:function(data){
+      			// console.log(data);
+      			td.attr('data-status',data.result.tc_status); 
+      		    if(data.result.tc_status==0){
+      		    	$(that).html('注销');
+      		    }else{
+                    $(that).html('启用');
+      		    }
+      		}
+      	})
+      })
    	 }
    });
 });
