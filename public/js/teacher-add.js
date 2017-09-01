a660dc3a3ac217c1c13c4034ea619ee5a3f69ea6
mylbest <948596867@qq.com>
2017-09-01 15:36:32 +0800

    实现表单验证插件

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language'],function($,template,util){
+define(['jquery','template','util','datepicker','language','validate'],function($,template,util){
  // 设置导航菜单选中
  util.setMenu('/teacher/list');
  // 获取编辑讲师的ID
@@ -28,20 +28,43 @@ define(['jquery','template','util','datepicker','language'],function($,template,
  	submitForm('/api/teacher/add');
  }
  // 实现表单提交功能
-    function submitForm(url){
-    $('#formBtn').click(function(){
-      $.ajax({
-        type : 'post',
-        url : url,
-        data : $('#formId').serialize(),
-        dataType : 'json',
-        success : function(data){
-          if(data.code == 200){
-            location.href = '/teacher/list';
-          }
-        }
-      });
-    });
-  }
+       function submitForm(url){
+      	 $('#formId').validate({
+           sendForm : false,
+           valid : function(){
+           console.log(12345);
+           },
+           description:{
+           	tc_name:{
+           		required:'用户名不能为空',
+           		valid:'用户名可以使用'
+           	},
+           	tc_pass:{
+           		required:'密码不能为空',
+           		pattern:'必须是6位数字',
+           		valid:'密码正确'
+           	},
+           	 tc_join_date : {
+           		required:'入职日期不能为空',
+           		valid:'日期有效'
+           	}
+           }
+      	});
+      }
+  //   function submitForm(url){
+  //   $('#formBtn').click(function(){
+  //     $.ajax({
+  //       type : 'post',
+  //       url : url,
+  //       data : $('#formId').serialize(),
+  //       dataType : 'json',
+  //       success : function(data){
+  //         if(data.code == 200){
+  //           location.href = '/teacher/list';
+  //         }
+  //       }
+  //     });
+  //   });
+  // }
   
 })
